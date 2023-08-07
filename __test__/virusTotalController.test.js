45795ca65d594198856c1a4c46677e1d56e8e0a7
Paul Michael <paulebi4eva@gmail.com>
2023-08-07 19:44:08 +0100

    corrected test file

--- __test__/virusTotalController.test.js
+++ __test__/virusTotalController.test.js
@@ -8,20 +8,22 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('virusTotalController', () => {
- it('should return scan results for a valid URL', (done) => {
-  const validUrl = 'https://thetrumpet.ng';
+  it('should return scan results for a valid URL', (done) => {
+    const validUrl = 'https://thetrumpet.ng';
 
-  chai
-    .request(app)
-    .get(`/api/virusTotal/scan?url=${encodeURIComponent(validUrl)}`)
-    .end((err, res) => {
-      expect(res).to.have.status(200);
-      expect(res.body.data).to.be.an('object');
-      expect(res.body.data).to.have.property('type').to.equal('analysis');
-      expect(res.body.data).to.have.property('id');
-      expect(res.body.data.links).to.have.property('self');
-      // Add more assertions based on the response from the API
+    chai
+      .request(app)
+      .get(`/api/virusTotal/scan?url=${encodeURIComponent(validUrl)}`)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.data).to.have.property('type').to.equal('analysis');
+        expect(res.body.data).to.have.property('id');
+        expect(res.body.data.links).to.have.property('self');
+        // Add more assertions based on the response from the API
 
-      done();
-    });
+        done();
+      });
+  });
 });
+

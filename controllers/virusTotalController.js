ae3904a7ac8040517d344115f4662b51b3cd040a
Paul Michael <paulebi4eva@gmail.com>
2023-08-25 20:27:36 +0100

    updating SafePass Backend with middlewares and more functionalities

--- controllers/virusTotalController.js
+++ controllers/virusTotalController.js
@@ -1,33 +1,24 @@
 const axios = require('axios');
 const dotenv = require('dotenv');
-
 dotenv.config(); // Load environment variables
 
-const scanURL = async (req, res) => {
-  const url = req.query.url;
-  if (!url) {
-    return res.status(400).json({ error: 'URL parameter is missing' });
+const apikey = process.env.VIRUSTOTAL_API_KEY;
+
+async function scanURL(url, apikey) {
+  const options = {
+    url: `https://www.virustotal.com/api/v3/url/scan?apikey=${apikey}`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url }),
+  };
+
+  const response = await axios(options);
+
+  if (response.statusCode === 200) {
+    return response.body;
+  } else {
+    throw new Error(`VirusTotal API returned status code ${response.statusCode}`);
   }
-
-  try {
-    const response = await axios.post(
-      'https://www.virustotal.com/api/v3/urls',
-      { url }, // Pass the URL to scan as a form parameter in the request body
-      {
-        headers: {
-          'x-apikey': process.env.VIRUSTOTAL_API_KEY,
-          'Content-Type': 'application/x-www-form-urlencoded', // Set the correct content type for a form-urlencoded request
-        },
-      }
-    );
-
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data from VirusTotal API' });
-  }
-};
-
-module.exports = {
-  scanURL,
-};
-
+}
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,7 @@
 // middleware/authMiddleware.js
+const jwt = require('jsonwebtoken'); // Import the jsonwebtoken library
+require('dotenv').config(); // Load environment variables
+
 function authMiddleware(req, res, next) {
   const userId = getTokenFromHeaders(req.headers.authorization);
 
@@ -8,4 +11,20 @@ function authMiddleware(req, res, next) {
   next(); // Pass control to the next middleware or route handler
 }
 
+function getTokenFromHeaders(authorizationHeader) {
+  if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+    const token = authorizationHeader.substring(7);
+    try {
+      // Verify the token using the JWT secret key from .env
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      return decoded.userId;
+    } catch (error) {
+      // Return null if token is invalid
+      return null;
+    }
+  }
+  return null;
+}
+
 module.exports = authMiddleware;
+
